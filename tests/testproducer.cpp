d01ac5ba19050a3f54cd84ac1850b77395087844
lopez <lopez@zola.dynalogic>
2014-03-12 16:05:30 +0100

    some more work on producer

--- tests/testproducer.cpp
+++ tests/testproducer.cpp
@@ -60,3 +60,25 @@ TEST_F(TestProducerEnoughForOneRound, OneProductionRoundDoneAfterEnoughTime)
     ASSERT_EQ(output_amount1, producer.inventory().amount(output_type1));
     ASSERT_FALSE(producer.isBusy());
 }
+
+TEST_F(TestProducer, ContinueProductionForTenCyclesIfEnoughMaterials)
+{
+    const std::size_t nbCycle = 10;
+    producer.inventory().add(input_type1, nbCycle * input_amount1);
+    producer.inventory().add(input_type2, nbCycle * input_amount2);
+    std::size_t tick = 1;
+    producer.update();
+    while(producer.isBusy())
+    {
+        while(producer.isBusy())
+        {
+            producer.update();
+            tick++;
+        }
+        producer.update();
+        tick++;
+    }
+    ASSERT_EQ(nbCycle * timeout + 1, tick);
+    ASSERT_EQ(1, producer.inventory().count());
+    ASSERT_EQ(nbCycle * output_amount1, producer.inventory().amount(output_type1));
+}
