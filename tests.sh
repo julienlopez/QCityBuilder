dc12f270ebd7216667a3fdce003842fb5bc1b3eb
julienlopez <julien.lopez51@gmail.com>
2015-01-28 18:15:01 +0100

    working on adding CI on travis-ci (#11)

--- tests.sh
+++ tests.sh
@@ -3,6 +3,8 @@
 OBJ_DIR=$PWD/obj_lib
 QMAKE=qmake
 
+DIR_OPTIONS="--base-directory $PWD/lib --directory $OBJ_DIR" #--directory obj_tests --directory tests
+
 function error_exit
 {
 	echo "$1" 1>&2
@@ -13,16 +15,23 @@ $QMAKE "COVERAGE=1" && make VERBOSE=1
 
 [ $? -ne 0 ] && error_exit "Compile step failed."
 
-lcov --zerocounters --directory $OBJ_DIR --base-directory $PWD/lib --no-external #> lcov.log 2> lcov.err
-lcov --capture --initial --directory $OBJ_DIR --base-directory $PWD/lib --no-external --output-file app.baseline #>> lcov.log 2>> lcov.err
+lcov --zerocounters $DIR_OPTIONS --no-external #> lcov.log 2> lcov.err
+lcov --capture --initial $DIR_OPTIONS --no-external --output-file app.baseline #>> lcov.log 2>> lcov.err
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD/bin/
 bin/tests
 
-echo "analyzing coverage data..."
+echo "analyzing coverage data...: lcov --no-checksum $DIR_OPTIONS --no-external --capture --output-file app"
+lcov --no-checksum $DIR_OPTIONS --no-external --capture --output-file app #>> lcov.log 2>> lcov.err
+#lcov --no-checksum $DIR_OPTIONS --no-external --capture --output-file app.info
 
-lcov --no-checksum --directory $OBJ_DIR --base-directory $PWD/lib --no-external --capture --output-file app #>> lcov.log 2>> lcov.err
+echo "merging datas"
 lcov --add-tracefile app.baseline --add-tracefile app --no-external --output-file app.info
+
+echo "removing useless data"
+lcov --remove app.info "/usr*" --output-file app.info
+
+echo "generation html doc"
 genhtml -o cov-html app.info #>> lcov.log 2>> lcov.err
 
 echo "done!"
