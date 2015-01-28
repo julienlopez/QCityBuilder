93f612c0383d1955af64d0936f21f80da7454d56
julienlopez <julien.lopez51@gmail.com>
2015-01-28 17:41:00 +0100

    working on adding CI on travis-ci (#11)
    Just a shot in the dark

--- tests.sh
+++ tests.sh
@@ -13,15 +13,16 @@ $QMAKE "COVERAGE=1" && make VERBOSE=1
 
 [ $? -ne 0 ] && error_exit "Compile step failed."
 
-lcov --zerocounters --directory $OBJ_DIR -b $PWD/lib --no-external #> lcov.log 2> lcov.err
-lcov --capture --initial --directory $OBJ_DIR -b $PWD/lib --no-external --output-file app #>> lcov.log 2>> lcov.err
+lcov --zerocounters --directory $OBJ_DIR --base-directory $PWD/lib --no-external #> lcov.log 2> lcov.err
+lcov --capture --initial --directory $OBJ_DIR --base-directory $PWD/lib --no-external --output-file app.baseline #>> lcov.log 2>> lcov.err
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD/bin/
 bin/tests
 
 echo "analyzing coverage data..."
 
-lcov --no-checksum --directory $OBJ_DIR -b $PWD/lib --no-external --capture --output-file app.info #>> lcov.log 2>> lcov.err
+lcov --no-checksum --directory $OBJ_DIR --base-directory $PWD/lib --no-external --capture --output-file app #>> lcov.log 2>> lcov.err
+lcov --add-tracefile app.baseline --add-tracefile app --no-external --output-file app.info
 genhtml -o cov-html app.info #>> lcov.log 2>> lcov.err
 
 echo "done!"
